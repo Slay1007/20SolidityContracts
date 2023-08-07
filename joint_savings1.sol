62baada85b265be32d63613e59e3b1638a282129
Slay1007 <127869955+Slay1007@users.noreply.github.com>
2023-08-07 10:58:19 -0400

    Initial commit

---
+++ joint_savings1.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+contract JointSavings {
+
+    address payable public accountOne;
+    address payable public accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+
+    function withdraw(uint amount, address payable recipient) public {
+        require(recipient == accountOne || recipient == accountTwo, "You don’t own this account!");
+        require(address(this).balance >= amount, "Insufficient funds!");
+
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
+
+        recipient.transfer(amount);
+        lastWithdrawAmount = amount;
+        contractBalance = address(this).balance;
+    }
+
+    function deposit() public payable {
+        // Update `contractBalance`
+        contractBalance = address(this).balance;
+    }
+
+    function setAccounts(address payable account1, address payable account2) public {
+        accountOne = account1;
+        accountTwo = account2;
+    }
+
+    // Default fallback function to receive Ether
+    function() external payable {
+        // Update `contractBalance`
+        contractBalance = address(this).balance;
+    }
+}
